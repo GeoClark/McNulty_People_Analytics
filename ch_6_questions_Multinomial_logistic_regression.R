b2a01053af97782e8a557a50e7535a96cfe84234
GeoClark <ckitclark@gmail.com>
2021-02-06 14:18:24 -0700

    created ch.6

---
+++ ch_6_questions_Multinomial_logistic_regression.R
@@ -0,0 +1,24 @@
+#Chapter 6 Data Exercises
+#5.5.2
+#multinomial Logistic Regression
+library(desc)
+library(peopleanalyticsdata)
+library(dplyr)
+library(ggplot2)
+library(tidyr)
+library(GGally)
+library(fastDummies)
+library(ggridges)
+library(LogisticDx)
+
+#6.5.2 Data exercises
+#Use the same health_insurance data set from this chapter to answer these questions.
+#
+#Complete the full stratified approach to modeling the three product choices that was started in 6.2. Calculate the coefficients, odds ratios and p-values in each case.
+#Carefully write down your interpretation of the odds ratios from the previous question.
+#Run a multinomial logistic regression model on the product outcome using Product B as reference. Calculate the coefficients, ratios and p-values in each case.
+#Verify that the coefficients for Product C against reference Product B matches those calculated in 6.3.3.
+#Carefully write down your interpretation of the odds ratios calculated in the previous question.
+#Use the process described in 6.4.1 to simplify the multinomial model in Question 4.
+
+
