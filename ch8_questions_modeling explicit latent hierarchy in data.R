b094abe2ca9d1eb668791b30ecc13f246bca48d3
GeoClark <ckitclark@gmail.com>
2021-02-16 21:10:02 -0700

    created file 2-16-21

---
+++ ch8_questions_modeling explicit latent hierarchy in data.R
@@ -0,0 +1,23 @@
+#Chapter 8
+#modeling explicit latent hierarchy in data
+
+library(dplyr)
+library(peopleanalyticsdata)
+
+#8.3.2.1
+#Split the data into two sets according to the gender of the participant. Run standard binomial logistic regression models on each set to determine the relationship between the dec decision outcome and the input variables samerace, agediff, attr, intel and prob.
+#
+#
+#
+#8.3.2.2
+#Run similar mixed models on these sets with a random intercept for iid.
+#
+#
+#
+#8.3.2.3
+#What different conclusions can you make in comparing the mixed models with the standard models?
+#
+#
+#  
+# 8.3.2.4 #
+#Experiment with some random slope effects to see if they reveal anything new about the input variables.
