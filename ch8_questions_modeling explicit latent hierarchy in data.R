b167f6c1ef84ac71333439fe6a4196fa4f106e2b
GeoClark <ckitclark@gmail.com>
2021-02-19 16:40:27 -0700

    Began chapter 8 questions. Speed dating example. Glm 2-19-21

--- ch8_questions_modeling explicit latent hierarchy in data.R
+++ ch8_questions_modeling explicit latent hierarchy in data.R
@@ -6,7 +6,29 @@ library(peopleanalyticsdata)
 
 #8.3.2.1
 #Split the data into two sets according to the gender of the participant. Run standard binomial logistic regression models on each set to determine the relationship between the dec decision outcome and the input variables samerace, agediff, attr, intel and prob.
+#load speed dating data
+speed_dating<-peopleanalyticsdata::speed_dating
+
+#split into two sets based on gender
+
+speed_dating_male<-filter(speed_dating, gender==1)
+speed_dating_fem<-filter(speed_dating, gender==0)
+
+# run standard binomial model for males
+    model_male <- glm(dec ~ agediff + samerace + attr + intel + prob, 
+                      data = speed_dating_male, 
+                      family = "binomial")
+# run standard binomial model for males
+
+    model_female <- glm(dec ~ agediff + samerace + attr + intel + prob, 
+                      data = speed_dating_fem, 
+                      family = "binomial")
+
+#get model summaries
+    summary(model_male)
+    summary(model_female)
 #
+
 #
 #
 #8.3.2.2
