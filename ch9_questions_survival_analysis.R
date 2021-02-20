7b0b8d50102bd0660953bb4f8acc826d53863daa
GeoClark <ckitclark@gmail.com>
2021-02-20 13:50:37 -0700

    added questions to file 2/20/21

--- ch9_questions_survival_analysis.R
+++ ch9_questions_survival_analysis.R
@@ -1,2 +1,39 @@
 #ch9_questions_survival_analysis
 webpage<- "http://peopleanalytics-regression-book.org/survival.html"
+
+# 9.4.2.1
+#Create three categories of intention as follows: High (score of 7 or higher), Moderate (score of 4–6), Low (score of 3 or less)
+
+
+
+# 9.4.2.2
+#Calculate Kaplan-Meier survival estimates for the three categories and visualize these using survival curves.
+
+
+
+
+# 9.4.2.3
+#Determine the effect of intention on retention using a Cox proportional hazard model, controlling for gender, field and level.
+
+
+
+
+# 9.4.2.4
+#Perform an appropriate check that the proportional hazard assumption holds for your model.
+
+
+
+
+# 9.4.2.5
+#Run a similar model but this time include the sentiment input variable. How would you interpret the results?
+
+
+
+
+# 9.4.2.6
+#Experiment with running a frailty model to take into account the different background attrition risk by field of employment
+
+
+
+
+
